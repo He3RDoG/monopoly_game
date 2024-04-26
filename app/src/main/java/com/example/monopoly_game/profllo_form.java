f1cd674b1606ac27a7eaf810ff178171251453b1
Andrea Monico <andrea.monico01@gmail.com>
2024-04-26 15:21:23 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ app/src/main/java/com/example/monopoly_game/profllo_form.java
@@ -0,0 +1,21 @@
+package com.example.monopoly_game;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class profllo_form  extends AppCompatActivity {
+
+
+
+
+    }
+
